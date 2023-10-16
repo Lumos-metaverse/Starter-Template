8456a237f3ac51da3c40b2f93d4dc4a4430df6c5
Vedant Chainani <vedantchainani1084@gmail.com>
2023-10-17 02:51:52 +0530

    remove content from homepage

--- src/components/common/navbar/index.tsx
+++ src/components/common/navbar/index.tsx
@@ -13,7 +13,7 @@ const Navbar = () => {
 			<div className='flex flex-row items-center justify-between'>
 				<div className='flex flex-row items-center gap-2'>
 					<PiAirplaneTakeoffDuotone className='text-4xl text-blue-500' />
-					<div className='hidden text-2xl font-bold sm:flex'>W3-Starter</div>
+					<div className='hidden text-2xl font-bold sm:flex'>Starter Template</div>
 				</div>
 				<div className='flex flex-row items-center gap-2'>
 					<ThemeSwitcher />
